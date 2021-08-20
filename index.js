67ff3244ca9cf0d6860725b838324ba46eb41397
zaZavier Ferodova Al Fitroh <zavierferodova@gmail.com>
2021-08-20 08:53:33 +0700

    Fix bootstrap error

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import 'regenerator-runtime'
-import 'bootstrap/dist/js/bootstrap'
 import bootstrapStyle from 'bootstrap/dist/css/bootstrap.css'
 import fontAwesomeStyle from './libraries/font-awesome/css/font-awesome.min.css'
 import './javascript/main.js'
